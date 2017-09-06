f8d2cf08c997a0704d06df28db03c6ee3826444e
york yao <yaoao12306@outlook.com>
2017-09-06 15:13:44 +0800

    improve script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,5 +1,7 @@
 const childProcess = require('child_process')
+const util = require('util')
 
+const execAsync = util.promisify(childProcess.exec)
 module.exports = {
   build: [
     'rimraf dist/',
@@ -14,19 +16,13 @@ module.exports = {
     'tsc -p spec',
     'jasmine',
     `node dist/index.js demo/test.md --css demo/style.css -o demo/test.pdf --spacing --html demo/test.html`,
-    () => new Promise((resolve, reject) => {
-      childProcess.exec('git status -s', (error, stdout, stderr) => {
-        if (error) {
-          reject(error)
-        } else {
-          if (stdout) {
-            reject(new Error('generated files does not match.'))
-          } else {
-            resolve()
-          }
-        }
-      }).stdout.pipe(process.stdout)
-    })
+    async () => {
+      const { stdout } = await execAsync('git status -s')
+      if (stdout) {
+        console.log(stdout)
+        throw new Error(`generated files doesn't match.`)
+      }
+    }
   ],
   fix: {
     ts: `tslint --fix "src/**/*.ts"`,
