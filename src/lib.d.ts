0d4ba487ad65fc45dfa447c43faca346380e9466
york yao <yaoao12306@outlook.com>
2017-09-27 15:04:10 +0800

    use @types/puppeteer

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -2,18 +2,6 @@ declare module "*.json" {
     export const version: string;
 }
 
-declare module "puppeteer" {
-    export function launch(): Promise<Browser>;
-    export class Browser {
-        newPage(): Promise<Page>;
-        close(): void;
-    }
-    export class Page {
-        setContent(html: string): Promise<void>;
-        pdf(options: { path: string }): Promise<void>;
-    }
-}
-
 declare module "pangu" {
     export function spacing(words: string): string;
 }
