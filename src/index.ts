fe3455b215936e660ec8435b73028a551f857572
york yao <yaoao12306@outlook.com>
2017-12-13 16:56:21 +0800

    feat: disable no-console by default

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,8 @@ import * as packageJson from "../package.json";
 
 let suppressError = false;
 
-function printInConsole(message: any) {
-    if (message instanceof Error) {
-        message = message.message;
-    }
-    // tslint:disable-next-line:no-console
-    console.log(message);
-}
-
 function showToolVersion() {
-    printInConsole(`Version: ${packageJson.version}`);
+    console.log(`Version: ${packageJson.version}`);
 }
 
 async function executeCommandLine() {
@@ -105,9 +97,13 @@ ${htmlContent}
 }
 
 executeCommandLine().then(() => {
-    printInConsole("markdown_to_pdf success.");
+    console.log("markdown_to_pdf success.");
 }, error => {
-    printInConsole(error);
+    if (error instanceof Error) {
+        console.log(error.message);
+    } else {
+        console.log(error);
+    }
     if (!suppressError) {
         process.exit(1);
     }
