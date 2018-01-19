85121f28df67ae8e4f2fc21686da5ba0f1def0de
york yao <yaoao12306@outlook.com>
2018-01-19 09:01:25 +0800

    refactor: use tslint-config-standard as lint rule

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,94 @@
-import * as minimist from "minimist";
-import * as puppeteer from "puppeteer";
-import * as MarkdownIt from "markdown-it";
-import * as hljs from "highlight.js";
-import * as fs from "fs";
-import * as pangu from "pangu";
-import * as markdownlint from "markdownlint";
-import * as packageJson from "../package.json";
+import * as minimist from 'minimist'
+import * as puppeteer from 'puppeteer'
+import * as MarkdownIt from 'markdown-it'
+import * as hljs from 'highlight.js'
+import * as fs from 'fs'
+import * as pangu from 'pangu'
+import * as markdownlint from 'markdownlint'
+import * as packageJson from '../package.json'
 
-let suppressError = false;
+let suppressError = false
 
-function showToolVersion() {
-    console.log(`Version: ${packageJson.version}`);
+function showToolVersion () {
+  console.log(`Version: ${packageJson.version}`)
 }
 
-async function executeCommandLine() {
-    const argv = minimist(process.argv.slice(2), { "--": true });
+async function executeCommandLine () {
+  const argv = minimist(process.argv.slice(2), { '--': true })
 
-    const showVersion = argv.v || argv.version;
-    if (showVersion) {
-        showToolVersion();
-        return;
-    }
+  const showVersion = argv.v || argv.version
+  if (showVersion) {
+    showToolVersion()
+    return
+  }
 
-    suppressError = argv.suppressError;
+  suppressError = argv.suppressError
 
-    const files = argv._;
-    if (files.length !== 1) {
-        throw new Error("Need 1 file.");
-    }
-    const file = files[0];
+  const files = argv._
+  if (files.length !== 1) {
+    throw new Error('Need 1 file.')
+  }
+  const file = files[0]
 
-    const pdfPath: string = argv.o;
-    if (!pdfPath) {
-        throw new Error("Need a pdf path(-o path).");
-    }
+  const pdfPath: string = argv.o
+  if (!pdfPath) {
+    throw new Error('Need a pdf path(-o path).')
+  }
 
-    const htmlPath: string = argv.html;
+  const htmlPath: string = argv.html
 
-    const cssPath: string = argv.css;
-    const cssContent = cssPath ? fs.readFileSync(cssPath).toString() : "";
+  const cssPath: string = argv.css
+  const cssContent = cssPath ? fs.readFileSync(cssPath).toString() : ''
 
-    const md = MarkdownIt({
-        highlight: (str: string, lang: string) => {
-            if (lang && hljs.getLanguage(lang)) {
-                try {
-                    return `<pre><code class="hljs ${lang}">${hljs.highlight(lang, str).value}</code></pre>`;
-                } catch (error) {
+  const md = MarkdownIt({
+    highlight: (str: string, lang: string) => {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return `<pre><code class="hljs ${lang}">${hljs.highlight(lang, str).value}</code></pre>`
+        } catch (error) {
                     // tslint:disable-next-line:no-console
-                    console.log(error);
-                }
-            } else {
-                try {
-                    return `<pre><code class="hljs">${hljs.highlightAuto(str).value}</code></pre>`;
-                } catch (error) {
-                    // tslint:disable-next-line:no-console
-                    console.log(error);
-                }
-            }
-            return `<pre><code class="hljs">${md.utils.escapeHtml(str)}</code></pre>`;
-        },
-    });
-
-    let fileContent = fs.readFileSync(file).toString();
-
-    const spacing: boolean = argv.spacing;
-    if (spacing) {
-        const newFileContent = pangu.spacing(fileContent);
-        if (newFileContent !== fileContent) {
-            fs.writeFileSync(file, newFileContent);
-            fileContent = newFileContent;
+          console.log(error)
         }
+      } else {
+        try {
+          return `<pre><code class="hljs">${hljs.highlightAuto(str).value}</code></pre>`
+        } catch (error) {
+                    // tslint:disable-next-line:no-console
+          console.log(error)
+        }
+      }
+      return `<pre><code class="hljs">${md.utils.escapeHtml(str)}</code></pre>`
     }
+  })
 
-    const lint: boolean = argv.lint;
-    if (lint) {
-        const lintResult = markdownlint.sync({
+  let fileContent = fs.readFileSync(file).toString()
+
+  const spacing: boolean = argv.spacing
+  if (spacing) {
+    const newFileContent = pangu.spacing(fileContent)
+    if (newFileContent !== fileContent) {
+      fs.writeFileSync(file, newFileContent)
+      fileContent = newFileContent
+    }
+  }
+
+  const lint: boolean = argv.lint
+  if (lint) {
+    const lintResult = markdownlint.sync({
             files: [file],
             config: {
-                "default": true,
-                "line-length": false,
-            },
-        }).toString();
-        if (lintResult) {
-            console.log(lintResult);
-            throw new Error("Lint error.");
-        }
+                'default': true,
+                'line-length': false
+            }
+        }).toString()
+    if (lintResult) {
+      console.log(lintResult)
+      throw new Error('Lint error.')
     }
+  }
 
-    const htmlContent = md.render(fileContent);
-    const styledHtmlContent = `<!DOCTYPE html>
+  const htmlContent = md.render(fileContent)
+  const styledHtmlContent = `<!DOCTYPE html>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="renderer" content="webkit" />
@@ -99,29 +99,29 @@ ${cssContent}
 <body>
 ${htmlContent}
 </body>
-`;
+`
 
-    if (htmlPath) {
-        fs.writeFileSync(htmlPath, styledHtmlContent);
-    }
+  if (htmlPath) {
+    fs.writeFileSync(htmlPath, styledHtmlContent)
+  }
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(styledHtmlContent);
-    await page.pdf({ path: pdfPath });
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setContent(styledHtmlContent)
+  await page.pdf({ path: pdfPath })
 
-    browser.close();
+  browser.close()
 }
 
 executeCommandLine().then(() => {
-    console.log("markdown_to_pdf success.");
+  console.log('markdown_to_pdf success.')
 }, error => {
-    if (error instanceof Error) {
-        console.log(error.message);
-    } else {
-        console.log(error);
-    }
-    if (!suppressError) {
-        process.exit(1);
-    }
-});
+  if (error instanceof Error) {
+    console.log(error.message)
+  } else {
+    console.log(error)
+  }
+  if (!suppressError) {
+    process.exit(1)
+  }
+})
