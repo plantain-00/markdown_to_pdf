6bc3efe24661e534e17e0f4e5274537902e793e9
york yao <yaoao12306@outlook.com>
2018-01-21 10:58:30 +0800

    chore: update dependencies

--- src/index.ts
+++ src/index.ts
@@ -46,14 +46,12 @@ async function executeCommandLine () {
         try {
           return `<pre><code class="hljs ${lang}">${hljs.highlight(lang, str).value}</code></pre>`
         } catch (error) {
-                    // tslint:disable-next-line:no-console
           console.log(error)
         }
       } else {
         try {
           return `<pre><code class="hljs">${hljs.highlightAuto(str).value}</code></pre>`
         } catch (error) {
-                    // tslint:disable-next-line:no-console
           console.log(error)
         }
       }
