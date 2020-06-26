135e093abbc109e80bf645210ccaa7a7a7d5e652
York Yao <me@yorkyao.com>
2020-06-26 10:47:10 +0800

    chore: remove commitlint

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -1,7 +1,6 @@
 import { checkGitStatus } from 'clean-scripts'
 
 const tsFiles = `"src/**/*.ts"`
-const jsFiles = `"*.config.js"`
 
 export default {
   build: [
@@ -10,9 +9,8 @@ export default {
     `node dist/index.js demo/test.md --css demo/style.css -o demo/test.pdf --spacing --html demo/test.html --lint`
   ],
   lint: {
-    ts: `eslint --ext .js,.ts,.tsx ${tsFiles} ${jsFiles}`,
+    ts: `eslint --ext .js,.ts,.tsx ${tsFiles}`,
     export: `no-unused-export ${tsFiles}`,
-    commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
     typeCoverage: 'type-coverage -p src --strict'
   },
@@ -20,5 +18,5 @@ export default {
     'clean-release --config clean-run.config.ts',
     () => checkGitStatus()
   ],
-  fix: `eslint --ext .js,.ts,.tsx ${tsFiles} ${jsFiles} --fix`
+  fix: `eslint --ext .js,.ts,.tsx ${tsFiles} --fix`
 }
